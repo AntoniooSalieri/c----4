61c91cc9ca2e37f4507eedd589f06679feea647f
AsuraVV <antonio.fon.salieri@gmail.com>
2024-11-17 13:35:21 +0200

    bucket filter tusk solved

---
+++ bucket_filter/Program.cs
@@ -0,0 +1,36 @@
+﻿
+int[,] picture = new int[,]
+{
+{0, 0, 0, 1, 1, 1, 0, 0},
+{0, 0, 1, 0, 0, 0, 1, 0},
+{0, 1, 0, 0, 1, 1, 0, 0},
+{0, 0, 1, 0, 1, 1, 0, 0},
+{0, 0, 1, 0, 0, 0, 1, 0},
+{0, 0, 0, 1, 1, 1, 0, 0},
+};
+
+Console.Clear();
+
+void Printer(){
+    for (int i = 0; i < picture.GetLength(0); i++){
+        for (int j = 0; j < picture.GetLength(1); j++){
+            if (picture[i, j] == 0) Console.Write("  ");
+            else Console.Write("+ ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Bucket(int str, int line){
+    if (picture[str, line] == 0){
+        picture[str, line] = 1;
+        Bucket(str-1, line);
+        Bucket(str, line-1);
+        Bucket(str+1, line);
+        Bucket(str, line+1);
+    }
+}
+
+Printer();
+Bucket(4, 4);
+Printer();
