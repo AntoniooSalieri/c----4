8b74f9500d96b75b9221a80853244cc06f35759d
AsuraVV <antonio.fon.salieri@gmail.com>
2024-11-17 14:43:44 +0200

    febonachi

---
+++ febonachi/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 0,
+    b = 1,
+    c;
+void febonachi(int count){
+    if (count>0){
+        count--;
+        Console.WriteLine(a);
+        c = a + b;
+        a = b;
+        b = c;
+        febonachi(count);
+    }
+}
+
+febonachi(20);
